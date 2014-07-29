eff53a43f6e1e0985879c367c714c0eb78046b6f
Siddarth Chaturvedi <siddarth49.pilani@gmail.com>
2014-07-29 20:33:28 +0530

    Yo Man Commits

---
+++ MyFolder/JFManTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  JFManTableViewCell.h
+//  Pods
+//
+//  Created by siddarth chaturvedi on 29/07/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JFManTableViewCell : UITableViewCell
+
+@end
